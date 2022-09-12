2c861b02072f0f7903828f0b6611a3c4d928d738
Павел Дровнин <pavel98rus@gmail.com>
2022-09-12 14:15:43 +0300

    yatube_final 1.0

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sorl.thumbnail',
+    'sorl.thumbnail',  # Приложение для картинок
 ]
 
 MIDDLEWARE = [
